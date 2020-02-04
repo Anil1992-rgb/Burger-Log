ac1f125d36f2c2a11f19ac9a894156ebe0a5441a
anil canbolat <alancturk1992@gmail.com>
2020-02-04 12:11:40 -0800

    added all necessary files and folder

---
+++ db/schema.sql
@@ -0,0 +1,14 @@
+### Schema
+
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	devour BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (name) VALUES ('Bomber');
+INSERT INTO burgers (name) VALUES ('Chicken');
+INSERT INTO burgers (name, devour) VALUES ('Cheese', true);
+INSERT INTO burgers (name, devour) VALUES ('Ultimate bbq', true);
+INSERT INTO burgers (name, devour) VALUES ('Big Mac', true);
+INSERT INTO burgers (name) VALUES ('veggie');
